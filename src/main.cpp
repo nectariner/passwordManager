aabdb44267f85c7f2cd1de6246470454ccd13141
nectariner <fredcook789@protonmail.com>
2020-10-23 18:11:17 +0100

    expanded on the logging feature, now grabs the line, also open config files in the test environment

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <fstream>
 #include "logging.h"
 
 int main() {
-    logging::log(logging::INFO, __func__, "Program just started");
-    logging::log(logging::INFO, __func__);
+    constexpr std::string_view g_applicationName { "passwordManager" };
+    
+    //testEnv data CHANGE FOR PRODUCTION
+    constexpr std::string_view g_initialisationFileLocation { "testEnv/passwordManager/passwordManagerrc" };
+    
+    logging::log(logging::INFO, __func__, __LINE__, "Program just started");
+    
+    //check if it's been setup initially (e.g. folder ~/.config/password_manager/passwordmaanger.rc exists);
+    std::ifstream configFile { g_initialisationFileLocation.data(), std::ios::in };
+    if (!configFile) {
+        logging::log(logging::ERROR, __func__, __LINE__, "Failed to open file");
+    }
+    else {
+        logging::log(logging::ERROR, __func__, __LINE__, "File opened successfully");
+    }
+
     return 0;
 }
